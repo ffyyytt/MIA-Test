709f2d4285993d61b0bcb0da5557ae76755f53d6
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-13 17:38:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 # L'argument '-l' est indispensable pour bénéficier des directives de votre .bashrc
  
 # On peut éventuellement placer ici les commentaires SBATCH permettant de définir les paramètres par défaut de lancement :
-#SBATCH --gres gpu:1
+#SBATCH --gres gpu:2
 #SBATCH --time 1-23:50:00
 #SBATCH --cpus-per-gpu 28
-#SBATCH --mem-per-cpu 3G
+#SBATCH --mem-per-cpu 4G
 #SBATCH --mail-type FAIL,END
 
 conda activate myenv
