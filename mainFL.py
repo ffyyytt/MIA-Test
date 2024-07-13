e6b7b331c90dd56b4d8175cd584648377fc26fec
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-13 23:34:56 +0200

    Update FL

--- mainFL.py
+++ mainFL.py
@@ -1,14 +1,13 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-import flwr as fl
 import numpy as np
 import tensorflow as tf
 
 from tqdm import *
 from model import *
 from attack import *
-from FLStrategy import *
+from myFL import *
 import cifar10 as data
 
 from sklearn.metrics import roc_auc_score
@@ -16,12 +15,18 @@ from sklearn.metrics import roc_auc_score
 rounds = 10
 localEpochs = 10
 strategy, AUTO = getStrategy()
+clientModels = []
 with strategy.scope():
-    initModel, preprocess = model_factory()
+    serverModel, preprocess = model_factory()
+    for i in range(data.__N_CLIENTS__):
+        clientModels.append(model_factory()[0])
+        clientModels[-1].compile(optimizer = "sgd", # ProxSGD(mu=config["proximal_mu"])
+                                 loss = {'output': tf.keras.losses.SparseCategoricalCrossentropy()},
+                                 metrics = {"output": [tf.keras.metrics.SparseCategoricalAccuracy()]})
 trainLoaders, validLoader = data.loadFLTrain(preprocess)
 miaData, miaLabels = data.loadMIAData(preprocess)
-client_resources = {"num_cpus": 8, "num_gpus": 1}
 
-yPred = trainFL(strategy, initModel, trainLoaders, validLoader, miaData, localEpochs, rounds, client_resources, data.__N_CLIENTS__, model_factory)
+doFL(clientModels, serverModel, trainLoaders, validLoader, localEpochs, avg_aggregate, rounds)
+yPred = serverModel.predict(miaData)
 scores = miaEntropy(yPred)
 print("AUC:", roc_auc_score(miaLabels, scores))
---
+++ myFL.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def doFL(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn, rounds):
+    for round in range(rounds):
+        yPred = do_one_round(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn)
+    return yPred
+
+def do_one_round(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn):
+    for i in range(len(server_model.trainable_variables)):
+        for j in range(len(client_models)):
+            client_models[j].trainable_variables[i].assign(server_model[i])
+
+    for i in range(len(client_models)):
+        client_models[i].fit(trainLoaders[i], verbose=False, epochs=local_epochs)
+    
+    aggregate_fn(server_model, client_models)
+    yPred = server_model.predict(validLoader, verbose=False)
+    return yPred
+
+def avg_aggregate(server_model, client_models):
+    for i in range(len(server_model.trainable_variables)):
+        server_model.trainable_variables[i].assign(sum([client_models.trainable_variables[j] for j in range(len(client_models))])/len(client_models))
