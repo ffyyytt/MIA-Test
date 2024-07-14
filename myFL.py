d295d3f9c4bc1612a3e8f892852ea1cd6cdd1b8d
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-14 04:04:02 +0200

    Update myFL.py

--- myFL.py
+++ myFL.py
@@ -21,6 +21,4 @@ def do_one_round(client_models, server_model, trainLoaders, validLoader, local_e
 
 def avg_aggregate(server_model, client_models):
     for i in range(len(server_model.trainable_variables)):
-        s = sum([client_models[j].trainable_variables[i] for j in range(len(client_models))])
-        print(s)
-        server_model.trainable_variables[i].assign(s/len(client_models))
+        server_model.trainable_variables[i].assign(sum([client_models[j].trainable_variables[i] for j in range(len(client_models))])/len(client_models))
