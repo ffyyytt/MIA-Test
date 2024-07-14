c4c803028e017f29931500df2e479edacce85a63
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-14 14:05:50 +0200

    Update myFL.py

--- myFL.py
+++ myFL.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-import keras.backend as K
+import tensorflow.keras.backend as K
 from tqdm import *
 
 def doFL(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn, rounds):
