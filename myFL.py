981f0c07d0459b9b4aa38f6283ed0e5eafe6b8d8
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-14 03:11:42 +0200

    Update myFL.py

--- myFL.py
+++ myFL.py
@@ -4,7 +4,7 @@ from tqdm import *
 def doFL(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn, rounds):
     for round in trange(rounds):
         yPred = do_one_round(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn)
-        print(np.mean(np.argmax(yPred, axis=1), validLoader.labels.flatten()))
+        print(np.argmax(yPred, axis=1), validLoader.labels.flatten())
     return yPred
 
 def do_one_round(client_models, server_model, trainLoaders, validLoader, local_epochs, aggregate_fn):
@@ -13,7 +13,7 @@ def do_one_round(client_models, server_model, trainLoaders, validLoader, local_e
             client_models[j].trainable_variables[i].assign(server_model.trainable_variables[i])
 
     for i in range(len(client_models)):
-        client_models[i].fit(trainLoaders[i], verbose=True, epochs=local_epochs)
+        client_models[i].fit(trainLoaders[i], validation_data = validLoader, verbose=True, epochs=local_epochs)
     
     aggregate_fn(server_model, client_models)
     yPred = server_model.predict(validLoader, verbose=False)
