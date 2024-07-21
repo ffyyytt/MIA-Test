0a46ba6c74c6d39d9ab72c87e8083fae961191e4
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-21 08:15:13 +0200

    Update result.py

--- result.py
+++ result.py
@@ -32,8 +32,8 @@ inIdx = np.where(inOutLabels==1)
 outIdx = np.where(inOutLabels==0)
 inSet = predictions[inIdx]
 outSet = predictions[outIdx]
-print(inSet)
-print(outSet)
+print(inSet, np.mean(inSet))
+print(outSet, np.mean(outSet))
 inScores = probabilityNormalDistribution(inSet, predictions[0])
 outScores = probabilityNormalDistribution(outSet, predictions[0])
 LiRAScores = [inScore/outScore for inScore, outScore in zip(inScores, outScores)]
