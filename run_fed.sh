d65bd221e4f6859ab71a737a7a4ccb8cc180448a
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-21 06:54:43 +0200

    Update

---
+++ run_fed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+# L'argument '-l' est indispensable pour bénéficier des directives de votre .bashrc
+ 
+# On peut éventuellement placer ici les commentaires SBATCH permettant de définir les paramètres par défaut de lancement :
+#SBATCH --gres gpu:1
+#SBATCH --time 1-23:50:00
+#SBATCH --cpus-per-gpu 9
+#SBATCH --mem-per-cpu 3G
+#SBATCH --mail-type FAIL,END
+#SBATCH --nodelist sn2
+
+conda activate myenv
+sequence=$(seq 0 256 | shuf)
+for i in $sequence
+do
+    python3 fed.py -data $i
+done
