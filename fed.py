f91947a87e8c234d6c324444535fc401cb960cd3
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-21 13:56:11 +0200

    AID

--- fed.py
+++ fed.py
@@ -8,7 +8,8 @@ parser.add_argument("-FL", help="Model backbone", nargs='?', type=str, default="
 parser.add_argument("-method", help="0: Federated Learning, 1: Federated Feature", nargs='?', type=int, default=0)
 parser.add_argument("-rounds", help="Rounds", nargs='?', type=int, default=5)
 parser.add_argument("-epochs", help="Local epochs", nargs='?', type=int, default=20)
-parser.add_argument("-data", help="Data index", nargs='?', type=int, default=0)
+parser.add_argument("-dataset", help="Dataset", nargs='?', type=str, default="cifar10")
+parser.add_argument("-index", help="Data index", nargs='?', type=int, default=0)
 parser.add_argument("-verbose", help="Verbose", nargs='?', type=bool, default=False)
 args = parser.parse_args()
 
@@ -19,9 +20,14 @@ from tqdm import *
 from model.model import *
 from attack.attack import *
 from model.FLStrategy import *
-import data.cifar10 as data
 
-from sklearn.metrics import roc_auc_score
+if args.dataset == "cifar10":
+    import data.cifar10 as data
+elif args.dataset == "AID":
+    import data.aid as data
+else:
+    print("Dataset not found")
+    import data.cifar10 as data
 
 if args.method == 0:
     aggregate = avg_aggregate
--- train.py
+++ train.py
@@ -4,20 +4,27 @@ import argparse
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 parser = argparse.ArgumentParser("YOLO")
-parser.add_argument("-data", help="Data index", nargs='?', type=int, default=0)
+parser.add_argument("-dataset", help="Dataset", nargs='?', type=str, default="cifar10")
+parser.add_argument("-index", help="Data index", nargs='?', type=int, default=0)
 parser.add_argument("-verbose", help="Verbose", nargs='?', type=bool, default=False)
 args = parser.parse_args()
 
 import tensorflow as tf
-import data.cifar10 as data
 
 from tqdm import *
 from model.model import *
 from attack.attack import *
-from sklearn.metrics import roc_auc_score
 
-if not os.path.isfile(f'{data.__FOLDER__}/cen/{args.data}.pickle'):
-    print(f"Centralized: {args.data} --------------------------------------")
+if args.dataset == "cifar10":
+    import data.cifar10 as data
+elif args.dataset == "AID":
+    import data.aid as data
+else:
+    print("Dataset not found")
+    import data.cifar10 as data
+
+if not os.path.isfile(f'{data.__FOLDER__}/cen/{args.index}.pickle'):
+    print(f"Centralized: {args.index} --------------------------------------")
     strategy, AUTO = getStrategy()
     with strategy.scope():
         model, preprocess = model_factory()
@@ -25,7 +32,7 @@ if not os.path.isfile(f'{data.__FOLDER__}/cen/{args.data}.pickle'):
                         loss = {'output': tf.keras.losses.SparseCategoricalCrossentropy()},
                         metrics = {"output": [tf.keras.metrics.SparseCategoricalAccuracy()]})
 
-    trainData, inOutLabels = data.loadCenData(args.data, preprocess)
+    trainData, inOutLabels = data.loadCenData(args.index, preprocess)
     model.fit(trainData, verbose = args.verbose, epochs = 100)
 
     miaData, validData = data.load(preprocess)
@@ -35,5 +42,5 @@ if not os.path.isfile(f'{data.__FOLDER__}/cen/{args.data}.pickle'):
     MIAPred = model.predict(miaData, verbose = args.verbose)
     print("MIA:", np.mean(miaData.labels.flatten() == np.argmax(MIAPred, axis = 1)))
 
-    with open(f'{data.__FOLDER__}/cen/{args.data}.pickle', 'wb') as handle:
+    with open(f'{data.__FOLDER__}/cen/{args.index}.pickle', 'wb') as handle:
         pickle.dump([MIAPred, inOutLabels], handle, protocol=pickle.HIGHEST_PROTOCOL)
