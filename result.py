14fbdcd549ea859858f70d70a1685a4e7d78432d
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-22 23:33:58 +0200

    Update result.py

--- result.py
+++ result.py
@@ -11,32 +11,33 @@ predictions = []
 inOutLabels = []
 (X_train, Y_train), (X_valid, Y_valid) = cifar10.load_data()
 Y_train_flatten = Y_train.flatten()
-files = glob.glob("cifar10/cen/*.pickle")
-print("n_shadow:", len(files))
-for f in tqdm(files):
-    data = pickle.load(open(f,'rb'))
-    predictions.append(data[0])
-    inOutLabels.append(data[1])
-    entropyScores = miaEntropy(data[0])
-    entropyModScores = miaEntropyMod(data[0], Y_train_flatten)
-    entropyAUC.append(roc_auc_score(data[1], entropyScores))
-    entropyModAUC.append(roc_auc_score(data[1], entropyModScores))
+for folder in glob.glob("cifar10/*"):
+    files = glob.glob(f"{folder}/*.pickle")
+    print("n_shadow:", len(files))
+    for f in tqdm(files):
+        data = pickle.load(open(f,'rb'))
+        predictions.append(data[1])
+        inOutLabels.append(data[2])
+        entropyScores = miaEntropy(data[1])
+        entropyModScores = miaEntropyMod(data[1], Y_train_flatten)
+        entropyAUC.append(roc_auc_score(data[2], entropyScores))
+        entropyModAUC.append(roc_auc_score(data[2], entropyModScores))
 
-print("Entropy:", np.mean(entropyAUC))
-print("Entropy Mod:", np.mean(entropyModAUC))
+    print(folder, "Entropy:", np.mean(entropyAUC))
+    print(folder, "Entropy Mod:", np.mean(entropyModAUC))
 
-predictions = np.array(predictions)
-inOutLabels = np.array(inOutLabels)
-predictions = predictions[:, np.arange(Y_train.shape[0]), Y_train_flatten]
-inIdx = np.where(inOutLabels==1)
-outIdx = np.where(inOutLabels==0)
-inSet = predictions[inIdx]
-outSet = predictions[outIdx]
-print(inSet, np.mean(inSet))
-print(outSet, np.mean(outSet))
-print(predictions[0][inOutLabels[0]==1])
-print(predictions[0][inOutLabels[0]==0])
-inScores = probabilityNormalDistribution(inSet, predictions[0])
-outScores = probabilityNormalDistribution(outSet, predictions[0])
-LiRAScores = [inScore/outScore for inScore, outScore in zip(inScores, outScores)]
-print("Entropy Mod:", roc_auc_score(data[1], LiRAScores))
+# predictions = np.array(predictions)
+# inOutLabels = np.array(inOutLabels)
+# predictions = predictions[:, np.arange(Y_train.shape[0]), Y_train_flatten]
+# inIdx = np.where(inOutLabels==1)
+# outIdx = np.where(inOutLabels==0)
+# inSet = predictions[inIdx]
+# outSet = predictions[outIdx]
+# print(inSet, np.mean(inSet))
+# print(outSet, np.mean(outSet))
+# print(predictions[0][inOutLabels[0]==1])
+# print(predictions[0][inOutLabels[0]==0])
+# inScores = probabilityNormalDistribution(inSet, predictions[0])
+# outScores = probabilityNormalDistribution(outSet, predictions[0])
+# LiRAScores = [inScore/outScore for inScore, outScore in zip(inScores, outScores)]
+# print("Entropy Mod:", roc_auc_score(data[1], LiRAScores))
