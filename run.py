f63e94320bd54ce4a60612a0c8beb76bbbfc310d
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-22 16:13:24 +0200

    Update run.py

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ for dataset in ["cifar10", "AID", "UCM"]:
     os.popen(f"mkdir {dataset}/cen").read()
     for FL in ["FedAvg", "FedProx"]:
         for method in range(3):
-            os.popen("sbatch run_fed.sh {dataset} {FL} {method}").read()
+            os.popen(f"sbatch run_fed.sh {dataset} {FL} {method}").read()
             os.popen(f"mkdir {dataset}/{FL}{'FT'*method}").read()
