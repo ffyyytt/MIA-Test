90b08f415041e09e4c0865ed75b3d5b5639b2149
Dương Anh Kiệt <dakiet.vn@gmail.com>
2024-07-22 16:11:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 
 for dataset in ["cifar10", "AID", "UCM"]:
+    os.popen(f"mkdir {dataset}").read()
     os.popen(f"sbatch run_cen.sh {dataset}").read()
     os.popen(f"mkdir {dataset}/cen").read()
     for FL in ["FedAvg", "FedProx"]:
